276ed993b55babe3d69fb52d1cb621bae59cf28e
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-03-31 23:14:01 +0600

    Home, Login, register, Main, Header component added

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,29 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Main from './Pages/Main/Main';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Register from './Pages/Register/Register';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
-    
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: "/login",
+        element: <Login></Login>
+      },
+      {
+        path: "/register",
+        element: <Register></Register>
+      }
+    ]
   },
 ]);
 
