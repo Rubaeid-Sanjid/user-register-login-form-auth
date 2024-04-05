b4e2068e49ecc94b02de29d02bb2b3bd869368be
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-04-06 00:09:07 +0600

    handle login functionality

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,10 +1,26 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import auth from "../../firebase/firebase.config";
 
 const Login = () => {
+    const handleLogin = (e) =>{
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        signInWithEmailAndPassword(auth, email, password)
+        .then(result=>{
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(error=>{
+            console.error(error.message);
+        })
+    }
     return (
         <div className="mt-12">
             <h2 className="text-3xl font-bold mb-4">Log in</h2>
             <div className="w-1/2 mx-auto p-6 bg-slate-400 rounded-3xl">
-                <form className="flex flex-col w-3/4 mx-auto">
+                <form onSubmit={handleLogin} className="flex flex-col w-3/4 mx-auto">
                     <input className="border-2 rounded-xl py-2 px-4" placeholder="Enter your email" type="email" name="email"/>
                     <input className="border-2 rounded-xl py-2 px-4 my-5" placeholder="Enter your password" type="password" name="password"/>
                     <input className="btn bg-blue-500 text-white rounded-xl py-2 px-4" type="submit" value="Login Now"/>
